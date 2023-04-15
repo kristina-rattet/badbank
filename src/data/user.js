110044a9b401be78837d5cd7a7ac01c79f576131
Kristina Rattet <kristina.rattet@gmail.com>
2023-04-15 01:18:25 -0700

    added 404 error page

---
+++ src/data/user.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    userPic: "/images/AccountPic0.png",
+    name: "New User 1",
+    email: "kbeaver@gmail.com",
+    password: "secret",
+    balance: 100,
+    transactionHistory: [],
+  },
+];
+
+export default users;
